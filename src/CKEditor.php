2a7639390db80a30c19579bab74c8b8a10cfeed3
Talemul Islam <talemul@mislbd.com>
2025-06-11 21:11:29 +0600

    first commit

---
+++ src/CKEditor.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SuperAdmin\Admin\CKEditor;
+
+use SuperAdmin\Admin\Extension;
+
+class CKEditor extends Extension
+{
+    public $name = 'ckeditor';
+
+    public $views = __DIR__.'/../resources/views';
+
+    //public $assets = __DIR__.'/../resources/assets';
+
+    public function assets()
+    {
+        return base_path('vendor/ckeditor/ckeditor');
+    }
+}
---
+++ src/CKEditorServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SuperAdmin\Admin\CKEditor;
+
+use Illuminate\Support\ServiceProvider;
+use SuperAdmin\Admin\Admin;
+use SuperAdmin\Admin\Form;
+
+class CKEditorServiceProvider extends ServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot(CKEditor $extension)
+    {
+        if (!CKEditor::boot()) {
+            return;
+        }
+
+        if ($views = $extension->views()) {
+            $this->loadViewsFrom($views, 'super-admin-ckeditor');
+        }
+
+        if ($this->app->runningInConsole() && $assets = $extension->assets()) {
+            $this->publishes(
+                [$assets => public_path('vendor/super-admin-ext/ckeditor')],
+                'super-admin-ckeditor'
+            );
+        }
+
+        Admin::booting(function () {
+            Admin::js('vendor/super-admin-ext/ckeditor/ckeditor.js', false); // prevent minifying (last arg)
+            Form::extend('ckeditor', Editor::class);
+        });
+    }
+}
